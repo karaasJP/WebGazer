2d3fdeb51f9be31a758c4d3e14a9e5115fb20275
karaasjp <bskzxc@gmail.com>
2018-11-28 15:15:30 +0900

    beautify

--- www/js/main.js
+++ www/js/main.js
@@ -1,18 +1,18 @@
-window.onload = function() {
+window.onload = function () {
 
     //start the webgazer tracker
     webgazer.setRegression('ridge') /* currently must set regression and tracker */
         .setTracker('clmtrackr')
-        .setGazeListener(function(data, clock) {
-          //   console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
-          //   console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
+        .setGazeListener(function (data, clock) {
+            //   console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
+            //   console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
         })
         .begin()
         .showPredictionPoints(true); /* shows a square every 100 milliseconds where current prediction is */
 
 
     //Set up the webgazer video feedback.
-    var setup = function() {
+    var setup = function () {
 
         //Set up the main canvas. The main canvas is used to calibrate the webgazer.
         var canvas = document.getElementById("plotting_canvas");
@@ -28,10 +28,10 @@ window.onload = function() {
             setTimeout(checkIfReady, 100);
         }
     }
-    setTimeout(checkIfReady,100);
+    setTimeout(checkIfReady, 100);
 };
 
-window.onbeforeunload = function() {
+window.onbeforeunload = function () {
     //webgazer.end(); //Uncomment if you want to save the data even if you reload the page.
     window.localStorage.clear(); //Comment out if you want to save data across different sessions
 }
@@ -39,7 +39,7 @@ window.onbeforeunload = function() {
 /**
  * Restart the calibration process by clearing the local storage and reseting the calibration point
  */
-function Restart(){
+function Restart() {
     document.getElementById("Accuracy").innerHTML = "<a>Not yet Calibrated</a>";
     ClearCalibration();
     PopUpInstruction();
