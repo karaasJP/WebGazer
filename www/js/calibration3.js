902f35cf89474a92ecb3a937c51068514fd4b73b
karaasjp <bskzxc@gmail.com>
2019-01-09 16:03:49 +0900

    キー押したときにdataが出るよ

--- www/js/calibration3.js
+++ www/js/calibration3.js
@@ -14,7 +14,6 @@ y[1] = y[2] = y[3] = 43;
 y[4] = y[5] = y[6] = 469;
 y[7] = y[8] = y[9] = 875;
 
-
 function handleKeydown(event) {
   // キーコード
   var keyCode = event.keyCode;
@@ -24,23 +23,26 @@ function handleKeydown(event) {
   if (48 <= keyCode && keyCode <= 90)
     target.innerHTML += String.fromCharCode(keyCode);
 
-  var row = keyCode - 48;
+  if (48 <= keyCode && keyCode <= 57) {
+    var row = keyCode - 48;
 
-  var elem = document.elementFromPoint(0, 0);
-  elem.dispatchEvent(new MouseEvent("click", {
-    clientX: x[row],
-    clientY: y[row]
-  }));
-  target.innerHTML += "<br>" + x[row] + " " + y[row];
+    var elem = document.elementFromPoint(0, 0);
+    elem.dispatchEvent(new MouseEvent("click", {
+      clientX: x[row],
+      clientY: y[row]
+    }));
+    target.innerHTML += "<br>" + x[row] + " " + y[row];
 
-  var calcolor = document.getElementById("Pt" + row);
-  times[row] += 1;
-  console.log(times[row]);
-  calcolor.style.opacity = String(0.2 * times[row] * 1.2);
-  if (times[row] >= 5) {
-    calcolor.style.backgroundColor = "yellow";
+    var calcolor = document.getElementById("Pt" + row);
+    times[row] += 1;
+    console.log(times[row]);
+    calcolor.style.opacity = String(0.2 * times[row] + 0.2);
+    if (times[row] >= 5) {
+      calcolor.style.backgroundColor = "yellow";
+    }
   }
 }
+
 
 window.addEventListener("mousemove", Coordinate);
 
